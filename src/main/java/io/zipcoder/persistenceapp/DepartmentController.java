6d10d9e61ddb42cc431423ceb855f5f770eecf3a
pknight <paul@zipcodewilmington.com>
2025-09-05 12:00:12 -0400

    Completed bullet 6

--- src/main/java/io/zipcoder/persistenceapp/DepartmentController.java
+++ src/main/java/io/zipcoder/persistenceapp/DepartmentController.java
@@ -8,6 +8,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/API/department")
 public class DepartmentController {
+    // Change the name of a department
+    @PutMapping("/{departmentNumber}/name")
+    public ResponseEntity<Department> updateDepartmentName(@PathVariable Long departmentNumber, @RequestParam String departmentName) {
+        Department department = departmentRepository.findOne(departmentNumber);
+        if (department == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        department.setDepartmentName(departmentName);
+        Department updated = departmentRepository.save(department);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
+    // Set a new department manager
+    @PutMapping("/{departmentNumber}/manager")
+    public ResponseEntity<Department> updateDepartmentManager(@PathVariable Long departmentNumber, @RequestParam Long managerId) {
+        Department department = departmentRepository.findOne(departmentNumber);
+        if (department == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        department.setManagerId(managerId);
+        Department updated = departmentRepository.save(department);
+        return new ResponseEntity<>(updated, HttpStatus.OK);
+    }
 
     @Autowired
     private DepartmentRepository departmentRepository;
